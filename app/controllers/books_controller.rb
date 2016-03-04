bd941314ec15cde9edcec606945b6c0451727fa7
kati taylor <katherine.pelletier@gmail.com>
2016-03-03 22:05:03 -0600

    frustration

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,21 @@
 class BooksController < ApplicationController
+
+
+  def self.all_lists(date)
+    date_obj = Date.parse(date) # taking date string in any form and turns into date object
+    str_date = date_obj.strftime("%Y-%m-%d") # once standardized, outputs as required
+    json = self.get("/#{str_date}/trade-fiction-paperback.json") # requests data from api with interpolated date
+    json["results"]["books"]
+  end
+
   def new
+   @books = Book.all_lists(date)
   end
 
   def index
-    @books = Book.all_lists
-    puts @books
+
   end
 
-  def show
-    @books = Book.find(params[:id])
-  end
+
 
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
-    @books = Book.all_lists("20110505")["results"]
   end
 
   def help
